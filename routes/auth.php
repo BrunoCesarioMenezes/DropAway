82608f37b6660abbaed7fba9320d45f8298a9736
BrunoCesarioMenezes <brunoagjf@gmail.com>
2025-12-24 15:09:20 -0300

    nao permite que um usuario logado acesse a pagina de login

--- routes/auth.php
+++ routes/auth.php
@@ -4,12 +4,13 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\Settings\PasswordController;
 use App\Http\Controllers\Settings\ProfileController;
 use App\Http\Controllers\Settings\TwoFactorAuthenticationController;
+use App\Http\Middleware\GuestMiddleware;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 
-Route::get('/login', function () {
+Route::middleware(GuestMiddleware::class)->get('/login', function () {
     return Inertia::render('login');
 })->name('login');
 
